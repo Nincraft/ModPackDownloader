5f050e3eb42eac21435b89901d48536a0efd8b1a
Nincodedo <nincodedo@gmail.com>
2016-02-15 23:57:43 -0600

    Add mod updater functionality WIP
    Lots of messy hardcoded stuff right now

---
+++ src/main/java/com/nincraft/modpackdownloader/ModUpdater.java
@@ -0,0 +1,113 @@
+package com.nincraft.modpackdownloader;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.nincraft.modpackdownloader.util.Reference;
+
+public class ModUpdater {
+
+	static Logger logger = LogManager.getRootLogger();
+
+	private static final String[] formats = { "yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ssZ",
+			"yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+			"yyyy-MM-dd HH:mm:ss", "MM/dd/yyyy HH:mm:ss", "MM/dd/yyyy'T'HH:mm:ss.SSS'Z'", "MM/dd/yyyy'T'HH:mm:ss.SSSZ",
+			"MM/dd/yyyy'T'HH:mm:ss.SSS", "MM/dd/yyyy'T'HH:mm:ssZ", "MM/dd/yyyy'T'HH:mm:ss", "yyyy:MM:dd HH:mm:ss", };
+
+	public static void updateCurseMods(String manifestFile, String mcVersion, String releaseType) {
+		try {
+			Long projectID;
+			Long fileID;
+			JSONParser parser = new JSONParser();
+			JSONObject jsons = (JSONObject) parser.parse(new FileReader(manifestFile));
+			JSONArray fileList = (JSONArray) jsons.get("curseFiles");
+			if (fileList == null) {
+				fileList = (JSONArray) jsons.get("files");
+			}
+			if (fileList != null) {
+				Iterator iterator = fileList.iterator();
+				while (iterator.hasNext()) {
+					JSONObject modJson = (JSONObject) iterator.next();
+					projectID = (Long) modJson.get("projectID");
+					fileID = (Long) modJson.get("fileID");
+					String url = Reference.CURSEFORGE_BASE_URL + projectID + Reference.COOKIE_TEST_1;
+					HttpURLConnection con = (HttpURLConnection) (new URL(url).openConnection());
+					con.setInstanceFollowRedirects(false);
+					con.connect();
+					String location = con.getHeaderField("Location");
+					String projectName = location.split("/")[2];
+					JSONParser projectParser = new JSONParser();
+					JSONObject projectJson = (JSONObject) projectParser.parse(new BufferedReader(new InputStreamReader(
+							new URL("http://widget.mcf.li/mc-mods/minecraft/237892-nincrafty-things.json")
+									.openStream())));
+					JSONObject fileListJson = (JSONObject) projectJson.get("files");
+					Date lastDate = null;
+					Long mostRecent = fileID;
+					for (Object thing : fileListJson.keySet()) {
+						JSONObject file = (JSONObject) fileListJson.get(thing);
+						Date date = parseDate((String) file.get("created_at"));
+						if (lastDate == null) {
+							lastDate = date;
+						}
+						if (lastDate.before(date) && file.get("type").equals(releaseType)
+								&& file.get("version").equals(mcVersion)) {
+							mostRecent = (Long) file.get("id");
+							lastDate = date;
+						}
+					}
+					logger.info("Most recent version of " + projectName + " is " + mostRecent+". Old version was "+fileID);
+					modJson.remove("fileID");
+					modJson.put("fileID", mostRecent);
+				}
+			}
+			FileWriter file = new FileWriter(manifestFile);
+			try {
+				file.write(jsons.toJSONString());
+			} finally {
+				file.flush();
+				file.close();
+			}
+
+		} catch (FileNotFoundException e) {
+			logger.error(e.getMessage());
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		} catch (ParseException e) {
+			logger.error(e.getMessage());
+		}
+	}
+
+	private static Date parseDate(String date) {
+		Date d = null;
+		if (date != null) {
+			for (String parse : formats) {
+				SimpleDateFormat sdf = new SimpleDateFormat(parse);
+				try {
+					d = sdf.parse(date);
+					logger.info("using format " + parse);
+				} catch (java.text.ParseException e) {
+
+				}
+			}
+		}
+		return d;
+	}
+
+}
