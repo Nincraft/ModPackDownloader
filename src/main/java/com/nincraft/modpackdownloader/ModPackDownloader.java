8090c5d757e4771c2ec265790849ac384276da89
Nincodedo <nincodedo@gmail.com>
2016-11-01 16:26:55 -0500

    Log arguments at startup

--- src/main/java/com/nincraft/modpackdownloader/ModPackDownloader.java
+++ src/main/java/com/nincraft/modpackdownloader/ModPackDownloader.java
@@ -16,12 +16,13 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.File;
+import java.util.Arrays;
 
 @UtilityClass
 @Log4j2
 public class ModPackDownloader {
 	public static void main(final String[] args) throws InterruptedException {
-
+		log.info("Starting ModPackDownloader with arguments: " + Arrays.toString(args));
 		initArguments(args);
 
 		if (Arguments.clearCache) {
