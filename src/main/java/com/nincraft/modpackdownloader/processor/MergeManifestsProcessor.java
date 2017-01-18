dee9918f7abc991547d9c98661497164d292e7d7
Nincodedo <nincodedo@gmail.com>
2017-01-18 01:32:03 -0600

    Remove unused import

--- src/main/java/com/nincraft/modpackdownloader/processor/MergeManifestsProcessor.java
+++ src/main/java/com/nincraft/modpackdownloader/processor/MergeManifestsProcessor.java
@@ -1,11 +1,10 @@
 package com.nincraft.modpackdownloader.processor;
 
+import com.nincraft.modpackdownloader.container.Manifest;
+
 import java.io.File;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-
-import com.nincraft.modpackdownloader.container.Manifest;
 
 public class MergeManifestsProcessor extends AbstractProcessor {
 	public MergeManifestsProcessor(final List<File> manifestFiles) {
