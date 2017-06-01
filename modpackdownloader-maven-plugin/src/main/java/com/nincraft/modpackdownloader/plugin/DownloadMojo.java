0c9dd27715a107864e62a91dfc419a0c0f0e5e9a
Nincodedo <nincodedo@gmail.com>
2017-05-31 22:47:22 -0500

    Initial major refactor

---
+++ modpackdownloader-maven-plugin/src/main/java/com/nincraft/modpackdownloader/plugin/DownloadMojo.java
@@ -0,0 +1,28 @@
+package com.nincraft.modpackdownloader.plugin;
+
+import com.nincraft.modpackdownloader.ModPackDownloader;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "download")
+public class DownloadMojo extends AbstractMojo {
+
+    @Parameter(property = "mpd.manifest", defaultValue = "manifest.json")
+    private String manifest;
+
+    @Parameter(property = "mpd.modfolder", defaultValue = "mods")
+    private String modFolder;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Starting modpack download");
+        String[] args = {"-manifest", manifest, "-modFolder", modFolder};
+        try {
+            ModPackDownloader.main(args);
+        } catch (InterruptedException e) {
+            getLog().error("Download interrupted", e);
+        }
+    }
+}
