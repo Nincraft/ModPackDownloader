366f241a877ee81f9080c4ce0d9ebe8893adfda7
Nincodedo <nincodedo@gmail.com>
2017-06-03 03:53:26 -0500

    Remove unused import

--- modpackdownloader-core/src/main/java/com/nincraft/modpackdownloader/util/Reference.java
+++ modpackdownloader-core/src/main/java/com/nincraft/modpackdownloader/util/Reference.java
@@ -3,7 +3,6 @@ package com.nincraft.modpackdownloader.util;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.experimental.UtilityClass;
 
 import static lombok.AccessLevel.PRIVATE;
 
